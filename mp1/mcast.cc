17dd6d0229a523ec41a5e23c051ff1646c212fe0
Joshua Harris <jharris4491@gmail.com>
2013-03-10 22:10:18 -0500

    Added Vector Time implementation and implemented member_join()

--- mp1/mcast.cc
+++ mp1/mcast.cc
@@ -1,12 +1,17 @@
 #include <string.h>
 #include <assert.h>
+#include <glib.h>
 
 #include "mp1.h"
 
+GHashTable TIMEKEEPER;
+int MEMBER_COUNT;
 
 
 void multicast_init(void) {
     unicast_init();
+    timekeeper = g_hash_table_new(NULL, NULL);
+    MEMBER_COUNT = 0;
 }
 
 /* Basic multicast implementation */
@@ -26,5 +31,50 @@ void receive(int source, const char *message, int len) {
 }
 
 void mcast_join(int member) {
+	MEMBER_COUNT ++;
 
+	int tKey = member;
+	int* tVector = create_vector();
+
+	int* keyList = g_hash_table_get_keys(TIMEKEEPER);
+	for(int i = 0; i < MEMBER_COUNT -1; i++){
+		int* currVector = g_hash_table_lookup(TIMEKEEPER, keyList[i]);
+		int* resultVector = expand_vector(currVector);
+		g_hash_table_replace(TIMEKEEPER, keyList[i], resultVector);
+	}
+
+	g_hash_table_insert(TIMEKEEPER, tKey, tVector);
+}
+
+/**
+	Auxilary function used to expand the vector when a new member is added to the chat.
+
+	arr - vector to be expanded
+	returns - new expanded vector with 0 as the newest entry
+*/
+int* expand_vector(int* arr) {
+	int* result = malloc(sizeOf(int) * MEMBER_COUNT);
+	
+	for(int i = 0; i < MEMBER_COUNT - 1; i++ ) {
+		result[i] = arr[i];
+	}
+	
+	delete[] arr;
+
+	result[MEMBER_COUNT - 1] = 0;
+
+	return result;
+}
+
+/*
+	Auxilary function for creating a new vector.
+
+	returns - a new vector initialized to 0
+*/
+int* create_vector() {
+	int* result = malloc(sizeOf(int) * MEMBER_COUNT);
+	for(int i = 0; i < MEMBER_COUNT; i ++)
+		result[i] = 0;
+
+	return result;
 }
